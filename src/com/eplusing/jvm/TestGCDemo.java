9a144edcf097a049f6330f27cf245c19900691c9
eeplusing <epluisng@gmail.com>
2019-04-18 23:48:53 +0800

    JVM GC参数设置

--- src/com/eplusing/jvm/TestGCDemo.java
+++ src/com/eplusing/jvm/TestGCDemo.java
@@ -6,21 +6,19 @@ public class TestGCDemo {
 
 	/**
 	 * JVM参数如下
-	 * -Xms16m -Xmx16m -XX:+PrintGCDetails
+	 * -Xms16m -Xmx16m -XX:+PrintGCDetails 
+	 * -Xms16m -Xmx16m -XX:+PrintGCDetails -XX:SurvivorRatio=4 (调整Eden与存活区比例4:3:3,默认8:1:1)
+	 * -Xms200m -Xmx200m -XX:+PrintGCDetails -XX:SurvivorRatio=4 -XX:PermSize=100m -XX:MaxPermSize=100m
 	 * C:\Program Files\Java\jdk1.8.0_144\bin\jvisualvm.exe图形界面查看内存使用及垃圾回收
 	 * jmap -heap 7436  查看java进程内存使用情况
 	 * */
 	public static void main(String[] args) {
 		Random rand = new Random();
 		String str = "www.eplusing.com";
+		System.out.println(1/0);
 		while(true){
-			/*try {
-				Thread.sleep(20);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}*/
-			str = str + rand.nextInt(100000);
+			
+			str = str + rand.nextInt(999999);
 			//将对象由年轻代放入永久代
 			str.intern();
 			
---
+++ src/com/eplusing/jvm/TestProcessDemo.java
@@ -0,0 +1,15 @@
+package com.eplusing.jvm;
+
+
+public class TestProcessDemo {
+
+	/**
+	 * */
+	public static void main(String[] args) {
+		//支持的进程数
+		System.out.println(Runtime.getRuntime().availableProcessors());
+	}
+	
+	
+
+}
