762fc5585d288ce8d4f0b8108ec86d018388c689
eeplusing <epluisng@gmail.com>
2019-04-21 13:26:44 +0800

    G1 强引用 软引用 弱引用 引用队列 幽灵引用

--- src/com/eplusing/jvm/TestGCDemo.java
+++ src/com/eplusing/jvm/TestGCDemo.java
@@ -9,13 +9,16 @@ public class TestGCDemo {
 	 * -Xms16m -Xmx16m -XX:+PrintGCDetails 
 	 * -Xms16m -Xmx16m -XX:+PrintGCDetails -XX:SurvivorRatio=4 (调整Eden与存活区比例4:3:3,默认8:1:1)
 	 * -Xms200m -Xmx200m -XX:+PrintGCDetails -XX:SurvivorRatio=4 -XX:PermSize=100m -XX:MaxPermSize=100m
+	 * 
+	 * 使用G1收集器
+	 * -Xms200m -Xmx200m -XX:+PrintGCDetails -XX:+UseG1GC
+	 * 
 	 * C:\Program Files\Java\jdk1.8.0_144\bin\jvisualvm.exe图形界面查看内存使用及垃圾回收
 	 * jmap -heap 7436  查看java进程内存使用情况
 	 * */
 	public static void main(String[] args) {
 		Random rand = new Random();
 		String str = "www.eplusing.com";
-		System.out.println(1/0);
 		while(true){
 			
 			str = str + rand.nextInt(999999);
