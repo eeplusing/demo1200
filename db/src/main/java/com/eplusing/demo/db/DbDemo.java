0295931693959465f6f86e87979455acb221ba49
eplusing@qq.com <150906Cpp>
2021-03-20 10:59:35 +0800

    事务的传播行为

--- db/src/main/java/com/eplusing/demo/db/DbDemo.java
+++ db/src/main/java/com/eplusing/demo/db/DbDemo.java
@@ -52,4 +52,15 @@ public class DbDemo {
      *
      * */
 
+    /**
+     * Spring事务的传播行为
+     * PROPAGATION_REQUIRED--支持当前事务，如果当前没有事务，就新建一个事务,最常见的选择。
+     * PROPAGATION_SUPPORTS--支持当前事务，如果当前没有事务，就以非事务方式执行。
+     * PROPAGATION_MANDATORY--支持当前事务，如果当前没有事务，就抛出异常。
+     * PROPAGATION_REQUIRES_NEW--新建事务，如果当前存在事务，把当前事务挂起, 两个事务之间没有关系，一个异常，一个提交，不会同时回滚
+     * PROPAGATION_NOT_SUPPORTED--以非事务方式执行操作，如果当前存在事务，就把当前事务挂起。
+     * PROPAGATION_NEVER--以非事务方式执行，如果当前存在事务，则抛出异常
+     *
+     * */
+
 }
