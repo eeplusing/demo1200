e57bfdda96ae5c756c2970363f837a7f90049cb3
eplusing@qq.com <150906Cpp>
2021-04-07 19:25:38 +0800

    demo

--- leetcode/src/test/java/org/leetcode/AppTest.java
+++ leetcode/src/test/java/org/leetcode/AppTest.java
@@ -1,5 +1,6 @@
 package org.leetcode;
 
+import com.eplusing.NC140;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -7,32 +8,69 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest
+        extends TestCase {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 3, 1, 4, 1000, 2, 300, 34, 2, 1, 1, 0};
+
+        //new NC140().bubbleSort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + ",");
+
+        }
+        System.out.println();
+
+        new NC140().quickSort(arr, 0, arr.length - 1);
+        //arr = new NC140().mergeSort(arr);
+        //new NC140().shellSort(arr);
+        //new NC140().insertSort(arr);
+
+        //new NC140().selectSort(arr);
+
+        //快速排序
+        //Arrays.sort(arr);
+
+
+        //Collections.sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + ",");
+        }
+
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+
+    //快速排序
+    public int[] quickSort(int[] arr, int start, int end) {
+        int base = arr[0];
+        int i = start;
+        int j = end;
+        while(i < j){
+            while(i < j && arr[j] > base){
+                j--;
+            }
+
+            while(i < j && arr[i] < base){
+                i++;
+            }
+
+            if(arr[i]== arr[j]){
+                i++;
+            }else{
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        if(i-1 > start){
+            quickSort(arr, start, i-1);
+        }
+        if(j - 1 < end){
+            quickSort(arr, j-1, end);
+        }
+
+        return arr;
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
