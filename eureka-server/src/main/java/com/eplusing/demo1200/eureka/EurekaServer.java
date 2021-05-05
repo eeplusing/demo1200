a192c60dcf7caa8da68cff6e8f8f45afb1b4ceb1
eplusing@qq.com <150906Cpp>
2021-05-05 10:00:12 +0800

    eureka server

--- eureka-server/src/main/java/com/eplusing/demo1200/eureka/EurekaServer.java
+++ eureka-server/src/main/java/com/eplusing/demo1200/eureka/EurekaServer.java
@@ -1,6 +1,7 @@
 package com.eplusing.demo1200.eureka;
 
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -9,6 +10,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaServer {
     public static void main(String[] args) {
-        new SpringApplicationBuilder(EurekaServer.class).run(args);
+        SpringApplication.run(EurekaServer.class, args);
     }
 }
--- eureka-server/src/test/java/com/eplusing/AppTest.java
+++ eureka-server/src/test/java/com/eplusing/AppTest.java
@@ -1,8 +1,7 @@
 package com.eplusing;
 
-import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
@@ -12,9 +11,4 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
 }
