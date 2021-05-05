f88610c06ae5b3c327494c22c36ced32c92a9ea3
eplusing@qq.com <150906Cpp>
2021-05-05 10:50:26 +0800

    app instance

---
+++ microservice/src/main/java/com/epluisng/demo1200/microservice/MicroserviceApplication.java
@@ -0,0 +1,14 @@
+package com.epluisng.demo1200.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceApplication.class, args);
+    }
+
+}
+
